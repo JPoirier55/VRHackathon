2ca96c2f1ceafde220e1ad586b26ed48f9970397
Jake <chhslabs@colostate.edu>
2016-10-22 10:49:25 -0600

    couple scripts

---
+++ Assets/Scripts/LightLine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightLine : MonoBehaviour {
+    public GameObject sphere1;
+   
+	// Use this for initialization
+	void Start () {
+        Debug.Log("World running script");
+        sphere1 = GameObject.Find("Sphere");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(sphere1.GetComponent<Renderer>().material.color);
+        //Debug.Log(Color.blue);
+        if(sphere1.GetComponent<Renderer>().material.color == Color.blue)
+        {
+            Debug.Log("Blue");
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.red);
+        }
+        else if (sphere1.GetComponent<Renderer>().material.color == Color.red)
+        {
+            Debug.Log("Red");
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+        }
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Color c;
+    GameObject sphere1;
+    GameObject sphere2;
+    GameObject sphere3;
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("Im alive");
+        InvokeRepeating("Change", 0.0f, 1.0f);
+        c = Color.red;
+        sphere1 = GameObject.Find("Sphere1");
+        sphere2 = GameObject.Find("Sphere2");
+        sphere3 = GameObject.Find("Sphere3");
+        Debug.Log(sphere1.name);
+        Debug.Log(sphere2.name);
+        Debug.Log(sphere3.name);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    void Change()
+    {
+        //Debug.Log(gameObject.tag);
+        
+        if(c == Color.red)
+        {
+            sphere1.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere2.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere3.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere1.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.red);
+            sphere2.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.yellow);
+            sphere3.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.green);
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+            c = Color.blue;
+
+        }
+        else if(c == Color.blue)
+        {
+            sphere1.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere2.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere3.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere1.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+            sphere2.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.green);
+            sphere3.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.red);
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.yellow);
+            c = Color.yellow;
+        }
+        else if (c == Color.yellow)
+        {
+            sphere1.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere2.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere3.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere1.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.yellow);
+            sphere2.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.red);
+            sphere3.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.green);
+            c = Color.green;
+        }
+        else if (c == Color.green)
+        {
+            sphere1.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere2.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere3.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            sphere1.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.green);
+            sphere2.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.blue);
+            sphere3.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.yellow);
+            gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+            gameObject.GetComponent<Renderer>().material.SetColor("_SpecColor", Color.red);
+            c = Color.red;
+        }
+    }
+}
